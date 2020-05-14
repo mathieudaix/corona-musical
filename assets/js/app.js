dea05d9766fcbc8b658ee9bfaa4a905eda931f2f
unknown <mathieudaixpro@gmail.com>
2020-05-14 16:48:26 +0200

    updatemusic

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,42 +4,45 @@ import gsap from 'gsap'
 document.addEventListener('DOMContentLoaded', () => {
 
     // navigation
-    const navTimeline = gsap.timeline({paused: true, reversed: true, defaults: {duration: .4, ease: 'expo.easeInOut'}})
+    const navTimeline = gsap.timeline({ paused: true, reversed: true, defaults: { duration: .4, ease: 'expo.easeInOut' } })
 
-    navTimeline.to(document.querySelector('.nav__menu'), {autoAlpha: 1}, 0)
-    navTimeline.to(document.querySelector('.nav__icon span:first-of-type'), {translateY: 3, rotate: -45}, 0)
-    navTimeline.to(document.querySelector('.nav__icon span:last-of-type'), {translateY: -3, rotate: 45}, 0)
+    navTimeline.to(document.querySelector('.nav__menu'), { autoAlpha: 1 }, 0)
+    navTimeline.to(document.querySelector('.nav__icon span:first-of-type'), { translateY: 3, rotate: -45 }, 0)
+    navTimeline.to(document.querySelector('.nav__icon span:last-of-type'), { translateY: -3, rotate: 45 }, 0)
 
     document.querySelector('.nav__icon').addEventListener('click', () => {
         navTimeline.reversed() ? navTimeline.play() : navTimeline.reverse()
     })
 
     // audio
-    const audio = document.querySelectorAll('.audio')
+    document.querySelectorAll('.audio').forEach(song => {
+        song.querySelector('.play').onclick = () => playMusic(song)
+        song.querySelector('.pause').onclick = () => pauseMusic(song)
+    })
 
-    for (let i = 0; i < audio.length; i++) {
-        const e = audio[i];
-        const music = new Audio(e.querySelector('audio').src)
+    function playMusic(song) {
+        const player = document.getElementById('player')
+        player.src = song.dataset.music
+        player.play()
+        song.querySelector('.play').style.display = 'none'
+        song.querySelector('.pause').style.display = 'block'
+        resetMusic(song)
+    }
 
-        e.querySelector('.play').addEventListener('click', () => {
-            audio.forEach(el => {
-               el.setAttribute('data-music', '0')
-                if (el.getAttribute('data-music') === '0') {
-                    el.querySelector('.play').style.display = 'block'
-                    el.querySelector('.pause').style.display = 'none'
-                }
-            })
-            e.setAttribute('data-music', '1')
-            e.querySelector('.play').style.display = 'none'
-            e.querySelector('.pause').style.display = 'block'
-            music.play()
-        })
+    function pauseMusic(song) {
+        const player = document.getElementById('player')
+        player.pause()
+        song.querySelector('.pause').style.display = 'none'
+        song.querySelector('.play').style.display = 'block'
+        resetMusic(song)
+    }
 
-        e.querySelector('.pause').addEventListener('click', () => {
-            e.setAttribute('data-music', '0')
-            e.querySelector('.pause').style.display = 'none'
-            e.querySelector('.play').style.display = 'block'
-            music.pause()
+    function resetMusic(song) {
+        document.querySelectorAll('.audio').forEach(songUI => {
+            if (songUI !== song) {
+                songUI.querySelector('.pause').style.display = 'none'
+                songUI.querySelector('.play').style.display = 'block'
+            }
         })
     }
 
